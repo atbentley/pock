4ed0793dae5f253a9dd0f5d0803f4dfa03831eaa
Andrew Bentley <andrew.t.bentley@gmail.com>
2016-10-04 21:45:33 +1100

    Version 0.0.2

--- pock/__init__.py
+++ pock/__init__.py
@@ -1,5 +1,5 @@
 from .helpers import mock, context_manager, when, verify, any_value, any_args, any_kwargs, any_values
 from .verification import VerificationError
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __all__ = ('mock', 'when', 'verify', 'any_value', 'any_args', 'any_kwargs', 'any_values', 'VerificationError')
