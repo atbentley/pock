92d2226580968c2d13ff9cf4562a29caf1df443d
Andrew Bentley <andrew.t.bentley@gmail.com>
2016-12-27 15:43:20 +1100

    Add async_context_manager to the top level of package

--- pock/__init__.py
+++ pock/__init__.py
@@ -1,4 +1,5 @@
-from .helpers import mock, context_manager, when, when_async, verify, any_value, any_args, any_kwargs, any_values
+from .helpers import (
+    mock, async_context_manager, context_manager, when, when_async, verify, any_value, any_args, any_kwargs, any_values)
 from .verification import VerificationError
 
 __version__ = '0.0.3'
